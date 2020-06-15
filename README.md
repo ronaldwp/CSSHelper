756d6889f52dd74d76224ac180e1212dcdd856f0
Ronaldwp <ronaldregan.waiphyo@gmail.com>
2020-06-15 13:12:01 +0630

    Final changes for V1.1

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # CSSHelper
 CSSHelper contains color generator, gradiant generator and box shadow generator, all in one. <a href="https://ronaldwp.github.io/CSSHelper/">Visit the page.</a>
+Hit me up if you have cool ideas or find bugs. glhf.
 
 Update Log
-V1.1
+V1.1 (62020)
 1. Choose up to three colors.
 2. Restructured color picker section.
+3. favicon added.
+4. Bookmark added.
 
-<!-- Future plan
+Future plan
 1. Inset option for box shadow.
 2. Some form of cheat sheet for css grid and flex-box.
-3. Bookmark icon and Favicon. -->
+
 
--- script.js
+++ script.js
@@ -97,3 +97,32 @@ function updateBox() {
 	// box.style.boxShadow = `${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${opacity.value/10})`;
 	boxShadowText.innerHTML = "box-shadow: " + textValue;
 }
+
+// bookmark
+function rudr_favorite(a) {
+  pageTitle=document.title;
+  pageURL=document.location;
+  try {
+    // Internet Explorer solution
+    eval("window.external.AddFa-vorite(pageURL, pageTitle)".replace(/-/g,''));
+  }
+  catch (e) {
+    try {
+      // Mozilla Firefox solution
+      window.sidebar.addPanel(pageTitle, pageURL, "");
+    }
+    catch (e) {
+      // Opera solution
+      if (typeof(opera)=="object") {
+        a.rel="sidebar";
+        a.title=pageTitle;
+        a.url=pageURL;
+        return true;
+      } else {
+        // The rest browsers (i.e Chrome, Safari)
+        alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
+      }
+    }
+  }
+  return false;
+}
